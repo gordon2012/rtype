8d559888ac1bdf4829569e021e33509cfd1c3227
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-02-08 07:30:00 -0800

    Fix image loading hang bug WIP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('file-loader?name=[name].[ext]!./index.html');
 require('./style.css');
 require('file-loader?name=[name].[ext]!./images/stars_small.png');
 require('file-loader?name=[name].[ext]!./images/player.png');
+require('file-loader?name=[name].[ext]!./images/debug.png');
 // TODO: Figure out better way of loading images
 
 
@@ -10,19 +11,39 @@ require('file-loader?name=[name].[ext]!./images/player.png');
 class ImageController {
     constructor() {
         this.images = {};
+        this.loaded = {};
     }
 
     setImage(name, src) {
         this.images[name] = new Image();
         this.images[name].src = src;
+        this.images[name].addEventListener('load', () => this.loaded[name] = true);
     }
 
     getImage(name) {
+        console.log(this.images[name]);
+        console.dir(this.images[name]);
         return this.images[name] || false;
     }
 
     logImage(name) {
         console.log(this.images[name]);
+    }
+
+    isLoaded() {
+        // console.log('IS LOADED');
+
+
+        // var keys = Object.keys(this.images);
+        // var check = false;
+        // check = keys.every((key) => this.loaded.hasOwnProperty(key));
+        // if(!check) console.log(check);
+        // return check;
+
+        return Object.keys(this.images).every((key) => this.loaded.hasOwnProperty(key));
+
+
+        // return false;
     }
 }
 
@@ -41,22 +62,24 @@ class Entity {
 }
 
 class Drawable extends Entity {
-    constructor(x, y, canvas, context) {
+    constructor(x, y, canvas, context, image) {
         super(x, y);
 
         this.canvas = canvas;
         this.context = context;
+        this.image = image;
     }
     draw() {
-        const player = imageController.getImage('player');
+        // const player = imageController.getImage('player');
+        const player = this.image;
 
         this.context.drawImage(player, this.x, this.y);
     }
 }
 
 class Background extends Drawable {
-    constructor(x, y, speed, canvas, context) {
-        super(x, y, canvas, context);
+    constructor(x, y, speed, canvas, context, image) {
+        super(x, y, canvas, context, image);
 
         this.speed = speed;
     }
@@ -68,8 +91,19 @@ class Background extends Drawable {
         this.x += this.speed.x;
         this.y += this.speed.y;
 
-        const bg = imageController.getImage('bg');
-        console.log(bg.width, bg.height);
+        //
+        // const bg = imageController.getImage('bg');
+        const bg = this.image;
+        
+
+
+        // if (bg === undefined) console.log('FALSE');
+        // console.log(bg.width, bg.height);
+
+        // console.log('DRAW BACKGROUND');
+        // this.context.drawImage(bg, this.x, this.y);
+
+        //*
         for(let i = -bg.width; i < this.canvas.width; i += bg.width) {
             for(let j = -bg.height; j < this.canvas.height; j += bg.height) {
                 this.context.drawImage(bg, this.x + i, this.y + j);
@@ -83,6 +117,7 @@ class Background extends Drawable {
             this.y = 0;
         if(this.y < 0)
             this.y = bg.height;
+        //*/
     }
 }
 
@@ -94,38 +129,96 @@ class Game {
         // Maybe put image loading here? Have imageController be an attribute of this object?
     }
 
+    run() {
+
+
+        // var p = new Promise(() => game.loadImages());
+        // p.then(() => console.log('IMAGES LOADED'));
+
+        this.loadImages();
+
+
+        if(this.init()) {
+            this.start();
+        }
+    }
+
     init() {
+        console.log('INIT GAME')
         if(this.bgCanvas.getContext) {
             this.bgContext = this.bgCanvas.getContext('2d');
 
-            this.background = new Background(0, 0, {x: -3, y: 1}, this.bgCanvas, this.bgContext);
-            this.player = new Drawable(128, 128, this.bgCanvas, this.bgContext);
+            this.background = new Background(0, 0, {x: -3, y: 1}, this.bgCanvas, this.bgContext, this.images.getImage('bg'));
+            this.player = new Drawable(128, 128, this.bgCanvas, this.bgContext, this.images.getImage('player'));
 
             return true;
         } else return false;
     }
 
+    loadImages() {
+        // Init images here?
+        //
+        console.log('LOADING IMAGES');
+        this.images = new ImageController();
+
+
+
+        this.images.setImage('bg', 'stars_small.png');
+        this.images.setImage('nasa', 'https://source.unsplash.com/CzigtQ8gPi4/1500x1500');
+        this.images.setImage('player', 'player.png');
+        this.images.setImage('debug', 'debug.png');
+
+    }
+
     start() {
+        console.log('START GAME')
         this.animate();
     }
 
     animate() {
         window.requestAnimationFrame(() => this.animate());
-        this.background.draw();
+        
+        if(this.images.isLoaded()) {
+            this.background.draw();
+            this.player.draw();
+        } else {
+            console.log('WAITING FOR IMAGE');
+        }
 
-        this.player.draw();
+
+
     }
 }
 
 
 // Initialize the game
 //
-const imageController = new ImageController();
-imageController.setImage('bg', 'stars_small.png');
+// const imageController = new ImageController();
+// imageController.setImage('bg', 'stars_small.png');
 // imageController.setImage('bg', 'https://source.unsplash.com/CzigtQ8gPi4/1500x1500');
+// imageController.setImage('player', 'player.png');
 
 
-imageController.setImage('player', 'player.png');
 
 const game = new Game();
-if(game.init()) game.start();
+game.run();
+
+// if(game.init()) {
+    
+//     var images = [
+//         {name: 'bg', url: 'https://source.unsplash.com/CzigtQ8gPi4/1500x1500'},
+//         {name: 'player', url: 'player.png'}
+//     ];
+
+//     Promise.all
+
+
+//     // var p = new Promise((res, rej) => game.loadImages());
+//     // p.then(() => console.log('IMAGES LOADED'));
+
+//     // Promise.resolve(() => game.loadImages())
+//     //     .then(() => console.log('IMAGES LOADED'));
+
+
+//     game.start();
+// }
